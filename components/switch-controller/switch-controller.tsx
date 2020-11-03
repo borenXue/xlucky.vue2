0e56b4f476372c833785d62303a8a8dd15f8261c
xueboren <xueboren001@gmail.com>
2020-11-03 17:50:48 +0800

    fix(FileDownload): 升级 xtools_js - header、formdata、data 的处理

--- components/switch-controller/switch-controller.tsx
+++ components/switch-controller/switch-controller.tsx
@@ -184,13 +184,13 @@ export default class SwitchController extends Vue {
           <path
             ref='pathA'
             style={this.styleSvgPath}
-            d='
+            d={`
               M 300 400
               L 700 400
               C 900 400 900 750 600 850
               A 400 400 0 0 1 200 200
               L 800 800
-            '
+            `}
           />
 
           <path ref='pathB' d='M 300 500 L 700 500' style={this.styleSvgPath} />
@@ -198,13 +198,13 @@ export default class SwitchController extends Vue {
           <path
             ref='pathC'
             style={this.styleSvgPath}
-            d='
+            d={`
               M 700 600
               L 300 600
               C 100 600 100 200 400 150
               A 400 380 0 1 1 200 800
               L 800 200
-            '
+            `}
           />
         </svg>
       </div>

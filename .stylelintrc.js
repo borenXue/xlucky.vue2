5edf99581a3e4f385f2755120ec1e09ddf0af4f2
boren.xue <boren.xue@outlook.com>
2021-05-28 18:08:35 +0800

    chore(build): 添加 css-loader、styleling-scss 支持

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   ignoreFiles: ['./**', '!./**/*.scss', '!./**/*.sass', '!./**/*.css'],
+  plugins: ['stylelint-scss'],
   extends: ['stylelint-config-standard'],
   rules: {
     // 这里可以覆盖一些配置
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
   },
 };
